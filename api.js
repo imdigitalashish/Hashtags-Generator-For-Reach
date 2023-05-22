89a35db11b26df127b887a6bc4168f5003ad78f6
imdigitalashish <imdigitalashish@gmail.com>
2023-05-22 15:00:43 +0530

    https://github.com/imdigitalashish/Hashtags-Generator-For-Reach

---
+++ api.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/api/tags', async (req, res) => {
+  const { query } = req.query;
+  const apiUrl = 'https://rapidtags.io/api/generator?query=' + query;
+
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(apiUrl);
+
+    // Wait for the content to load if necessary
+    // For example, if the website fetches data dynamically
+    // you can use `page.waitForSelector` or `page.waitForTimeout`
+
+    const content = await page.evaluate(() => {
+      return JSON.parse(document.querySelector('body').textContent);
+    });
+
+    await browser.close();
+
+    res.send(content);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'An error occurred' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
